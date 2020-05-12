9b4e044d3d2a2b0a19eb10e248f36826d2dee1a5
gaoxinit <gaoxinit@163.com>
2020-05-12 19:37:24 +0800

    在家下载看能不能运行

---
+++ src/main/java/cn/gaoxinit/test_run/TestRunApplication.java
@@ -0,0 +1,13 @@
+package cn.gaoxinit.test_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRunApplication.class, args);
+    }
+
+}
