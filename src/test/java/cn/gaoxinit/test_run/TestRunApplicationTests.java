5e420c403efdff43a212a904f1f221520147d922
gaoxinit <gaoxinit@163.com>
2020-05-12 19:41:37 +0800

    在家下载看能不能运行

---
+++ src/test/java/cn/gaoxinit/test_run/TestRunApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gaoxinit.test_run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
